74b3463265a74f74b078685a4e309ae2bb225595
Vitor Atair <vitor.ruts@gmail.com>
2014-09-27 11:55:40 -0300

    added a btn to set a default product

--- production/core/urls.py
+++ production/core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = patterns('production.core.views',
+	url(r'^setDefaultProduct/$', 'setDefaultProduct', name='core'),
 	url(r'^testBoard/$', 'testBoard', name='core'),
 	url(r'^testStatus/$', 'testStatus', name='core'),
     url(r'', 'home', name='core'),
--- production/core/views.py
+++ production/core/views.py
@@ -1,19 +1,38 @@
 #coding: utf-8
 
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 import commands
 import json
 import time
 from production.core.production_commands import *
-from django.contrib.auth.decorators import login_required
+from production.product.models import Product
+
 
 @login_required(login_url='/')
 def home(request):
 	"""
-		Must be return the index template
+		Must be return the index template or test template if already has a default product on the system
 	"""
 
-	return render(request, 'index_first.html')
+	if Product.objects.filter(default=1).count() == 0:
+		products = Product.objects.all()
+		return render(request, 'index_first.html', {'products':products})
+
+	return render(request, 'test.html')
+
+
+def setDefaultProduct(request):
+	"""
+		Must be set a new product as default
+	"""
+
+	product = request.POST['product']
+	Product.objects.all().update(default=0)
+	Product.objects.filter(pk=product).update(default=1)
+
+	return HttpResponseRedirect('/core/')
+
 
 def testBoard(request):
 	"""
