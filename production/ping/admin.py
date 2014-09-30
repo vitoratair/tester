5d81dd081f3de1bb70919f0ee56ac746528fc157
Vitor Atair <vitor.ruts@gmail.com>
2014-09-29 21:40:19 -0300

    update models and css to app ping

--- production/ping/admin.py
+++ production/ping/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from production.ping.models import Ping
 
+
 class PingAdmin(admin.ModelAdmin):
-	list_display = ('id', 'description')
+    list_display = ('id', 'name', 'description', 'ipOrigin',
+                    'ipDestination', 'interval', 'packetNumber', 'setByUser', 'result')
 
 
 admin.site.register(Ping, PingAdmin)
--- production/ping/models.py
+++ production/ping/models.py
@@ -6,19 +6,24 @@ from django.utils.translation import ugettext_lazy as _
 
 class Ping(models.Model):
 
+	resultOptions = (
+	    ('UNTESTED', 'Untested'),
+	    ('PASS', 'Approved into the test'),
+	    ('REPROVED', 'Failed into the test'),
+    )
 
 	name = models.CharField(_(u'Ping Name'), max_length=100, unique=True)
-	description = models.CharField(_(u'Ping description'), max_length=300, blank=True)
-	ipOrigin = models.IPAddressField(_(u'IP Origin'), blank=True,
+	description = models.CharField(_(u'Ping description'), max_length=300, blank=True, null=True)
+	ipOrigin = models.IPAddressField(_(u'IP Origin'), blank=True, null=True,
 		help_text='Origin IP address, without it, origin IP will be the local ip')
-	ipDestination = models.IPAddressField(_(u'IP Destination'),
+	ipDestination = models.IPAddressField(_(u'IP Destination'), null=True,
 		help_text='Destination IP address')
 
-	interval = models.IntegerField(_(u'Interval'), blank=True)
-	packetNumber = models.IntegerField(_(u'Packet Number'), blank=True)
-	setByUser = models.CharField(_(u'Made by User'), max_length=200, blank=True,
+	interval = models.IntegerField(_(u'Interval'), null=True, blank=True)
+	packetNumber = models.IntegerField(_(u'Packet Number'), null=True, blank=True)
+	setByUser = models.CharField(_(u'Made by User'), max_length=200, null=True, blank=True,
 		help_text='Comment all made by user')
-
+	result = models.CharField(_(u'Expected result'), max_length=10, choices=resultOptions, default='UNTESTED', blank=True)
 
 	class Meta:
 		ordering = ['id']
