dace3eac23615656ac4c7e74b0a929923dd6180d
Vitor Atair <vitor.ruts@gmail.com>
2014-10-14 19:51:32 -0300

    added a new defines

--- production/core/headers.py
+++ production/core/headers.py
@@ -1,5 +1,17 @@
 # coding: utf-8
 
+# Generic
+DEFAULT = 1
+NOT_DEFAULT = 2
+LOCALHOST = '127.0.0.1'
+
+###########################
+
+# Communication by
+SSH = 'SSH'
+CGI = 'CGI'
+
+###########################
 
 ### Indices of all mmodules
 PING = 1
@@ -10,7 +22,7 @@ IPERF = 2
 ### Paginator
 PAGE_LIMIT = 10
 
-#############
+###########################
 
 ### Module Product
 ACTIVE_VALUE = 'status: active'
@@ -18,4 +30,11 @@ NOT_FIND = -1
 BOARD_NOT_FOUND = "BOARD_NOT_FOUND"
 BOARD_FOUND = "BOARD_FOUND"
 
-##################
+###########################
+
+### Module Ping
+PASSED = 0
+REPROVED = 1
+DEFAULT_PACKET_NUMBER = 3
+
+###########################
